3dbaca02a3767f7e741b158b953f77c99165e04b
ChristophThiel <christoph.thiel@liwest.at>
2018-05-25 13:28:51 +0200

    Setup REST

---
+++ AlarmReport.Web/Controllers/AlarmsController.cs
@@ -0,0 +1,15 @@
+﻿using AlarmReport.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlarmReport.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class AlarmsController : Controller
+    {
+        [HttpGet]
+        public Alarm Get()
+        {
+            return new Alarm("Brandmeldealarm");
+        }
+    }
+}
---
+++ AlarmReport.Web/Models/Alarm.cs
@@ -0,0 +1,12 @@
+﻿namespace AlarmReport.Web.Models
+{
+    public class Alarm
+    {
+        public string AlarmKeyword { get; set; }
+
+        public Alarm(string alarmKeyword)
+        {
+            this.AlarmKeyword = alarmKeyword;
+        }
+    }
+}
